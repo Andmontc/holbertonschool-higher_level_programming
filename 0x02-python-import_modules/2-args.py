9fec8162a18701930408343d1e4344fa2f1c3181
Andmontc <oscarandresmontes@gmail.com>
2020-04-30 18:04:11 +0000

    print args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    x = len(sys.argv)
+    if(x == 0 or x == 1):
+        print("0 arguments.")
+    elif x == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(x - 1))
+    for args in range (x - 1):
+        print("{}: {}".format(args + 1, sys.argv[args + 1]))
