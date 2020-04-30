6cc4f9766bb4d87787effd52da0e0a517af4a563
Andmontc <oscarandresmontes@gmail.com>
2020-04-30 18:36:27 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    x = len(sys.argv)
+    suma = 0
+    for n in range (1, x):
+        suma += int(sys.argv[n])
+    print("{}".format(suma))
