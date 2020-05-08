6c6adf0076217b8ab06a7496670e908386c66f47
Andmontc <oscarandresmontes@gmail.com>
2020-05-08 02:54:04 +0000

    roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romansnum = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    if (not isinstance(roman_string, str) or roman_string is None):
+        return 0
+    num = 0
+    prev = 0
+    for i in roman_string:
+        if (prev < romansnum[i]):
+            num += (romansnum[i] - prev * 2)
+        elif prev >= romansnum[i]:
+            num += romansnum[i]
+        prev = romansnum[i]
+    return num
