1d6123d0b3ee8aeb387e37154ad69bb1b89a11af
Andmontc <oscarandresmontes@gmail.com>
2020-05-08 03:07:42 +0000

    average tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list is None or my_list = []:
         return 0
     mul = 0
     div = 0
     for i in my_list:
-        mul += i[0] * i[1]
+        mul += (i[0] * i[1])
         div += i[1]
     return mul / div
