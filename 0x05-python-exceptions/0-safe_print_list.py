1e40b1bd859fef050c689acadf9080625413697f
Andmontc <oscarandresmontes@gmail.com>
2020-05-18 17:29:21 +0000

    print with try and except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for i in my_list[:x]:
+        try:
+            j += 1
+            print("{}".format(i), end="")
+        except:
+            break
+    print("")
+    return j
