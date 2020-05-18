9678894834cea45098109f58b8063a103be27b06
Andmontc <oscarandresmontes@gmail.com>
2020-05-18 18:49:58 +0000

    safe print with except errors

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in my_list[:x]:
+        try:
+            print("{:d}".format(i), end="")
+            j += 1
+        except (TypeError, ValueError, IndexError):
+            continue
+    print()
+    return j
