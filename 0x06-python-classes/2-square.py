afb561368eb498c80a0cc9d35a4435e415fe2ad6
Andmontc <oscarandresmontes@gmail.com>
2020-05-19 21:31:52 +0000

    raise exception in class attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Class Square """
+
+
+class Square:
+    """ Attributes of the class """
+    def __init__(self, size=0):
+        """init method
+    size = size of the square, with try an exception """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
