3a8b72a7886514fe8ef148dfd81d908bbd6da801
Andmontc <oscarandresmontes@gmail.com>
2020-05-20 02:35:20 +0000

    print square with tuple coordinate

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,12 +33,11 @@ class Square:
     @position.setter
     def position(self, value):
         """ set/change the position value """
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if not isinstance(value[0], int) or value[0] < 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if not isinstance(value[1], int) or value[1] < 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
         self.__position = value
 
     def area(self):
