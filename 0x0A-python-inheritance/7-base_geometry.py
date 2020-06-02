6d8072d6815471e09752519aacdc357767e7af77
Andmontc <oscarandresmontes@gmail.com>
2020-06-02 23:50:39 +0000

    validate values

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
             Args: name and value
             Raises: Errors"""
         if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
