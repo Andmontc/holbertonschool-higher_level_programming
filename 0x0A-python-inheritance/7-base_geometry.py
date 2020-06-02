ec32ce12a90978b5413617025e912c105c78fac4
Andmontc <oscarandresmontes@gmail.com>
2020-06-02 22:21:59 +0000

    validate values

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """ Exception area not defined """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates values """
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
