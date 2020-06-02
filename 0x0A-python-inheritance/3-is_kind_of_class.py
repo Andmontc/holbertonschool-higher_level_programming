ea9cf3b31ba49d27de793c81192f5ab14e8e5331
Andmontc <oscarandresmontes@gmail.com>
2020-06-02 20:54:49 +0000

    instance of an object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def is_kind_of_class(obj, a_class):
+    """ function that compare the instance of an object
+        Args: obj = object
+              a_class = instance
+        Return: true if is instance, false if not """
+
+    if not isinstance(obj, a_class):
+        return False
+    return True
