32cdec4eb561667f25686521cf632d49e7254538
Andmontc <oscarandresmontes@gmail.com>
2020-06-03 01:32:35 +0000

    Rebel class

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Rebel Class"""
+
+
+class MyInt(int):
+    """ Class with inverted == an !="""
+    def __ne__(self, other):
+        """Override != opeartor with =="""
+        return super().__eq__(other)
+
+    def __eq__(self, other):
+        """Override == opeartor with !="""
+        return super().__ne__(other)
