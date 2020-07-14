f7d4de1ecf89b33b5498d153ba69dff2bacd512e
Andmontc <oscarandresmontes@gmail.com>
2020-07-14 21:50:40 +0000

    create database and table with primary key and unique id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- script that creates a database with a table
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT, 
+name VARCHAR (256) NOT NULL);
