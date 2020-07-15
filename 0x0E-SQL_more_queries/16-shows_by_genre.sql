ea3e11a99b749411ffffd79093312f0711dcd913
Andmontc <oscarandresmontes@gmail.com>
2020-07-15 02:40:17 +0000

    All shows and all genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that list all shows and all genres linked to that show contained in hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title, tv_genres.name;
