142421a290603ff1a9cabaddb074fff2c6b9f58e
Andmontc <oscarandresmontes@gmail.com>
2020-08-24 21:04:17 +0000

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     req = urllib.request.Request(url)
     try:
-        urllib.request.urlopen(req)
+        with urllib.request.urlopen(req) as resp:
+            print(resp.read().decode('ascii'))
     except urllib.error.HTTPError as e:
         print(Error code: e.code)
