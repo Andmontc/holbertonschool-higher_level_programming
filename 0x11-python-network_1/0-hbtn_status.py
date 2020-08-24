11af482f9b329e250c99d551c55ebcd33ea788f5
Andmontc <oscarandresmontes@gmail.com>
2020-08-24 19:50:28 +0000

    Script that fetch a page

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Script that fetch a page """
-import urllib.request
+"""python script that fetch a page"""
 
 if __name__ == '__main__':
+    import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body Response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: ' + str(type(html)))
+        print('\t- content: ' + str(html))
+        print('\t- utf8 content: ' + html.decode("utf-8"))
