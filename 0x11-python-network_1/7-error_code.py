0ce426d93267d910703cf888e85924f214ae18f1
Andmontc <oscarandresmontes@gmail.com>
2020-08-24 21:56:57 +0000

    script that takes in a URL and show and error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  script that takes in a URL and an email, sends a POST request """
+"""  script that takes in a URL and show and error code """
 import requests
 import sys
 
