3e40d22f32edac0bc0b1283ce10065aea58b514f
Andmontc <oscarandresmontes@gmail.com>
2020-08-24 21:33:31 +0000

    script that takes in a URL and an email, sends a POST request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""  script that takes in a URL and an email, sends a POST request """
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    r = requests.post(url, data=value)
+    print(r.text)
