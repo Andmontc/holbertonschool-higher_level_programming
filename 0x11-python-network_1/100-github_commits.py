09024257b94c24032ec8a7f0141fa4b879dcf710
Andmontc <oscarandresmontes@gmail.com>
2020-08-25 03:16:10 +0000

    script that consume de Api from github and print the 10 last commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  script that consume de Api from github and print the 10 last commits"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+           sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    commit = r.json()
+    for i in range(10):
+        print("{}: {}".format(commit[i].get('sha'),
+                              commit[i].get('commit').get('author')
+                              .get('name')))
