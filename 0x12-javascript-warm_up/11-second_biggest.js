0ab1288f6defefa409562a52d7758596725c30ae
Andmontc <oscarandresmontes@gmail.com>
2020-09-15 19:57:33 +0000

    Script that find the max number in arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const arg = process.argv.length;
 if (arg <= 3) {
   console.log(0);
 } else {
-  const maxnum = process.argv.slice(2);
+  const maxnum = parseInt(process.argv.slice(2));
   maxnum.sort();
-  console.log(parseInt(maxnum[maxnum.length - 2]));
+  console.log(maxnum[maxnum.length - 2]);
 }
